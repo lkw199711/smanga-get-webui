c6aa7a80c2fd2750c8f574d39cf73f9f6f44b0dd
ext.liangkaiwen8 <ext.liangkaiwen8@jd.com>
2026-06-03 12:00:11 +0800

    feat: 允许域名.

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       port: Number(env.VITE_PORT) || 9799,
       strictPort: true,
+      allowedHosts: true,
       proxy: {
         '/api': {
           target: apiTarget,
