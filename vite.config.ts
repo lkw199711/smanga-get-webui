f0d714072fe90fd30ff71c7565bd38aeefe8b56b
ext.liangkaiwen8 <ext.liangkaiwen8@jd.com>
2026-06-03 11:38:18 +0800

    feat: 使用env端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
   server: {
     host: '0.0.0.0', // 允许所有网络接口访问
-    port: 9799,      // 指定端口（默认可能为 5173 或 3000）
+    port: Number(env.VITE_PORT) || 9799,      // 从环境变量读取端口，默认 9799
     strictPort: true // 严格绑定指定端口
   },
   plugins: [
@@ -20,4 +22,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+}
 })
